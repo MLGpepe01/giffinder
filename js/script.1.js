55d3e05d414987c151ce4ea22d8955a2891b90bd
David Sliva <sliva@citypolyhs.org>
2018-03-06 23:27:44 +0000

    start project

---
+++ js/script.1.js
@@ -0,0 +1,10 @@
+// *************************** YOUR CODE BELOW *******************************
+//******************TEST EARLY AND OFTEN USING console.log() ******************
+//****************** SERIOUSLY TEST USING console.log()!!! ******************
+
+$(document).ready(function(){
+  
+  
+  
+  
+});
